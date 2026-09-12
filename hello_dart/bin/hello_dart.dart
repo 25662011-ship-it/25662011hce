6ec70c5249fa3d3f17e735090b282dc97602b22f
25662011-ship-it <25662011@kthcm.edu.vn>
2026-09-12 01:48:49 +0000

    dong bo 12/9

---
+++ hello_dart/bin/hello_dart.dart
@@ -0,0 +1,9 @@
+import 'package:hello_dart/hello_dart.dart' as hello_dart;
+
+void main(List<String> arguments) {
+  var ten = 'minh truong hce'
+  print('Hello, $truong! day la chuong trinh dau tien.');
+  for (var i=1;i<=5;i++){
+    print('Dem: $i');
+  }
+}
